20714bdf7391334ba56a4adaa9ae74b9f89176fc
jackasher36 <jackasher36@gmail.com>
2024-05-21 16:02:39 +0800

    Inital SpringMVC

---
+++ src/main/java/org/example/controller/FirstController.java
@@ -0,0 +1,8 @@
+package org.example.controller;/**
+ * @author Jackasher
+ * @version 1.0
+ * @className FirstController
+ * @since 1.0
+ **/
+public class FirstController {
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;/**
+ * @author Jackasher
+ * @version 1.0
+ * @className AppTest
+ * @since 1.0
+ **/
+public class AppTest {
+}
